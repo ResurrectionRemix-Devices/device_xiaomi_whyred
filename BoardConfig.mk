74d5de2426ddea74e63e6b142fd9d904676da3b1
TheScarastic <warabhishek@gmail.com>
2019-01-30 18:16:42 +0530

    whyred: Switch to more generic dt2w node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,7 +206,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event2"
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
