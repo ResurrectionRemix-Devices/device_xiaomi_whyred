a4889dda400c38be665e9963e37e4e45ae5e344f
baalajimaestro <baalajimaestro@computer4u.com>
2019-07-10 00:03:19 +0530

    s/aosp/rr
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- rr_whyred.mk
+++ rr_whyred.mk
@@ -1,4 +1,4 @@
-# 
+#
 # Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,9 +28,9 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := rr_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
