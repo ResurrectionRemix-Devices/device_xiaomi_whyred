0c55ebac9a58be1807dc600fa8281fc20bc046fb
celtare21 <celtare21@gmail.com>
2019-08-06 21:48:16 +0530

    whyred: vendor_prop: Squashed tweaks
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -8,8 +8,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m \
+		dalvik.vm.dex2oat-threads=8 \
     vendor.video.disable.ubwc=1 \
     persist.radio.multisim.config=dsds \
+		persist.radio.add_power_save=1 \
     persist.vendor.qcomsysd.enabled=1 \
     ro.vendor.extension_library=libqti-perfd-client.so \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
@@ -66,3 +68,24 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.hw.binder.size_kbyte=1024
+
+# GPU
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=c2d \
+    debug.mdpcomp.idletime=600 \
+    persist.hwc.mdpcomp.enable=true \
+    persist.hwc.ptor.enable=true \
+    debug.enable.sglscale=1 \
+    debug.sf.hw=1 \
+    debug.hwui.renderer=opengl \
+    debug.egl.hw=1 \
+    debug.sf.disable_hwc=0 \
+    debug.sf.recomputecrop=0 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1 \
+    persist.hwc.mdpcomp.enable=true \
+    persist.hwc.ptor.enable=true \
+    debug.sf.gpu_comp_tiling=1 \
+    debug.performance.tuning=1 \
+    video.accelerate.hw=1
